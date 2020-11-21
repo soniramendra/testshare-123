1ce0fe3ccef088306a779e8965fbb80615d18ea9
Ramendra Soni <soniramendran@gmail.coml>
2020-11-21 21:53:34 +0530

    initial

---
+++ routes/download.js
@@ -0,0 +1,19 @@
+const ruter = require('express').Router();
+const File = require('../models/file');
+const router = require('./files');
+
+
+
+router.get('/:uuid', async (req, res) => {
+    const file = await File.findOne({ uuid: req.params.uuid });
+    if(!file){
+        return res.render('download', {errr: 'Link has been expired..'});
+    }
+
+
+    const filePath = `${__dirname}/../${file.path}`;
+    res.download(filePath);
+});
+
+
+module.exports =  router;
